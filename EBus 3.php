d991e947501b86bc86b3b78f3105163d1696fb88
118314736 <118314736@BISB61VL.bis.ad.ucc.ie>
2019-03-07 09:27:48 +0000

    First Commit

---
+++ EBus 3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <body>
+<!-- //Starting the session to get the session variable from the last page-->
+<?php
+session_start();
+$totalValue2 = $_POST['txtTotal'];
+$fullNameValue = $_POST['txtName'];
+echo"The name is: "FullNameValue.".";
+echo"<br></br>";
+echo "The total value is: ".$totalValue2.".";
+?>
+        
+    </body>
+</html>
+        
---
+++ eBus2.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+$fullNmaeValue = "";
+$_totalValue2 = "";
+/*
+ * Create a session variable for the mobile number
+ */
+$totalValue = $_POST['txtTotal'];
+        $_SESSION['txtName'] = $fullNameValue;
+        $_SESSION['txtTotal'] = $totalValue2; 
+        ?>
+         
+/* ?>
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+        <title>eBusiness 2</title>
+        <body>
+        <div class="form">
+        <form name="Details" method="post" action= "eBus3.php">
+        <center>
+        <table cellspacing="10">
+        <tr>
+        <td><b></b></td>
+        <td><b>Enter in your details below</b></td>
+        </tr>
+        <td>Name</td>
+        <td><input type="text" id="txtName" name="txtName" value="" /></td>
+        </tr>
+        <td><input type="text" id="txtNum" name="txtNum" value="" /></td>
+        </tr>
+        <td>Password</td>
+        <td><input type="text" id="txtTotal" name="txtTotal" value="<?php echo $totalValue;?>" /></td>
+        </tr>
+        </table>
+        </center>
+        
+        <center>
+        
+        <input type="submit" name="btnContinue" id="btnContine" onclick="" value="Continue" /></td>
+        </center>
+        </div>
+        </form>
+        </body>
+        </html>
+
+        
+       
