0ffab6c32ca400c46739f3331d5b2768028c914b
118403782 <118403782@BISB61VL.bis.ad.ucc.ie>
2019-03-08 10:35:46 +0000

    Final commit

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -11,7 +13,51 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo 'Hello World' ; 
+            echo '' ; 
+        ?>
+    </body>
+</html>
+<html>
+    <head>
+        <title>Menu</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <style>
+            body {
+  
+  background-image:url("paper.jpg"); 
+}
+h1{text-align: center;
+}
+h2 {text-align:center;
+    color: red;
+}
+            
+        </style>
+    </head>
+    <body>
+        <h1>Menu</h1>
+        
+        <h2 class="text-white mb-5"><a href = http://localhost:8000/MyCV.html> My Cv</h2>
+        <h2 class="text-white mb-5"><a href = http://localhost:8000/Interests.html> My Interests</h2>
+        <h2 class="text-white mb-5"><a href = http://localhost:8000/newhtml1.html> Consulting Services</h2>
+        
+    </body>
+</html>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo '' ; 
         ?>
     </body>
 </html>
